d371903a38ef977d5a0412aac723996ef510064e
miguelvitta <miguelvittadev@gmail.com>
2024-12-17 23:43:05 -0300

    a simple program

---
+++ section3/section3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int myFavoriteNumber = 0;
+    
+    printf("Please enter your favorite number:");
+    scanf("%d", &myFavoriteNumber);
+    printf("You entered the number: %d\n", myFavoriteNumber);
+    
+    return 0;
+}

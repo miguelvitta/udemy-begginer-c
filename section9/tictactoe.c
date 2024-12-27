10086ea2c19e57eb5ec9ce9e6f8962c949f3798d
miguelvitta <miguelvittadev@gmail.com>
2024-12-27 12:35:16 -0300

    made good progress, let's save it

--- section9/tictactoe.c
+++ section9/tictactoe.c
@@ -1,23 +1,74 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-void drawBoard();
+void cleanInputBuffer();
+void drawBoard(char *board);
+void markBoard(char *board, bool *isPlayer1);
+int getValidInput();
+bool checkPlace(char *board, int place);
+
 
 int main() {
-    int placar1 = 0;
-    int placar2 = 0;
     bool choice = true;
+    bool isPlayer1 = true;
     char board[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     while (choice) {
-        printf("This is a game of tictactoe. Do you want to play a new game?\n(y/n): ");
+        printf(
+            "This is a game of tictactoe. Do you want to play a new "
+            "game?\n(y/n): ");
         getchar();
-        
+        cleanInputBuffer();
+        drawBoard(board);
+        markBoard(board, &isPlayer1);
     }
+    return 0;
+}
+
+void cleanInputBuffer() {
+    int ch = 0;
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        // Keep reading until a newline or EOF is encountered
+    }
+}
+
+void drawBoard(char *board) {
+    printf("\n\n\t\t\tTic Tac Toe\n\n\t\tPlayer 1 (X)   -   Player 2 (O)\n\n");
+    for (int i = 0, j = 0; i < 12; i++) {
+        if (i == 1 || i == 5 || i == 9) {
+            printf("\t%c\t|\t%c\t|\t%c\t\n", board[j], board[j + 1],
+                   board[j + 2]);
+            j += 3;
+        } else {
+            if (i == 3 || i == 7) {
+                printf(
+                    "----------------------------------------------------------"
+                    "\n");
+            } else {
+                printf("\t \t|\t \t|\t \t\n");  // line without numbers
+            }
+        }
+    }
+    printf("\n\n");
+}
+
+void markBoard(char *board, bool *isPlayer1) {
+    int place = 0;
+    bool valid = false;
+    while (!valid) {
+        place = getValidInput();
+    }
+    *isPlayer1 = !(*isPlayer1);
+}
+
+int getValidInput() {
 
 }
-      
-void drawBoard() {
-    printf("\t\tTic Tac Toe\n\nPlayer 1 (X)   -   Player 2 (O)\n\n");
 
+bool checkPlace(char *board, int place) {
+    if (board[place - 1] == 'X' || board[place - 1] == 'O') {
+        printf("Invalid place, this position was already chosen.\n");
+        return false;   
+    }
+    return true;
 }

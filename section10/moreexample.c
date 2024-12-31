b89fb7e36a69ddd5489090afb0f2b83dd2f49b99
miguelvitta <miguelvittadev@gmail.com>
2024-12-31 10:14:40 -0300

    actually completed the moreexample.c basic version

--- section10/moreexample.c
+++ section10/moreexample.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     double value = 0;
-    char *
+    char str[] = "3.5 2.5 1.26";
+    char *pstr = str;
+    char *ptr = NULL;
+
+    while (1) {
+        value = strtod(pstr, &ptr);
+        if(pstr == ptr) {
+            break;
+        }
+        else {
+            printf(" %f", value);
+            pstr = ptr;
+        }
+    
+    }
 
     return 0;
 }

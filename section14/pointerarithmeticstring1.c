87678bec69da1b9231c6dd44e9d79e190d3fb0e1
miguelvitta <miguelvittadev@gmail.com>
2025-01-12 18:29:25 -0300

    forgot to pass the code through the linter

--- section14/pointerarithmeticstring1.c
+++ section14/pointerarithmeticstring1.c
@@ -2,10 +2,12 @@
 
 int main() {
     long int multiple[] = {15, 25, 35, 87};
-    long int * p = multiple;
+    long int* p = multiple;
 
-    for (long unsigned int i = 0; i < sizeof(multiple)/sizeof(multiple[0]); ++i) {
-        printf("adress p+%ld (&multiple[%ld]): %llu *(p+%ld) value: %ld\n", i, i, (unsigned long long int)(p+i), i, *(p+i));
+    for (long unsigned int i = 0; i < sizeof(multiple) / sizeof(multiple[0]);
+         ++i) {
+        printf("adress p+%ld (&multiple[%ld]): %llu *(p+%ld) value: %ld\n", i,
+               i, (unsigned long long int)(p + i), i, *(p + i));
     }
 
     printf("\nType long occupies: %lu bytes\n", sizeof(long));

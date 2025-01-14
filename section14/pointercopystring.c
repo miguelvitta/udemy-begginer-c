1b1c42cf1d0715352d6e1cddd902ecf730669c86
miguelvitta <miguelvittadev@gmail.com>
2025-01-14 11:01:50 -0300

    learning to copy strings with pointers

---
+++ section14/pointercopystring.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 1001
+
+void getInput(char *str);
+void copyString1(char to[], char from[]);
+void copyString2(char *to, char *from);
+
+int main() {
+    char stringsa[SIZE];
+
+    getInput(stringsa);
+    char string1[SIZE];
+    copyString1(string1, stringsa);
+    printf("\n\n%s\n\n", string1);
+
+    char stringsb[SIZE];
+    getInput(stringsb);
+    char string2[SIZE];
+    copyString2(string2, stringsb);
+    printf("\n\n%s\n\n", string2);
+    return 0;
+}
+
+void getInput(char *str) {
+    printf("Write your string(1-1000): ");
+    if (fgets(str, SIZE - 1, stdin) != NULL) {
+        str[strcspn(str, "\n")] = '\0';
+    }
+}
+
+void copyString1(char to[], char from[]) {
+    int i = 0;
+    for (; from[i] != '\0'; ++i) {
+        to[i] = from[i];
+    }
+    to[i] = '\0';
+}
+
+void copyString2(char *to, char *from) {
+    char *ptr = to;
+    for (; *from != '\0'; ++ptr, ++from) {
+        *ptr = *from;
+    }
+    *ptr = '\0';
+}
+

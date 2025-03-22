765c3bd800d513eaf2774a8c37e9e74326e658d6
miguelvitta <miguelvittadev@gmail.com>
2025-03-22 09:09:04 -0300

    feature: a basica program that reads a file, and the corresponding file

--- section17/exampleio1.c
+++ section17/exampleio1.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
 int main() {
+    int x = FOPEN_MAX;
 
+    printf("The max number of files that can be open at the same time is: %d\n", x);
 
+    FILE* fp;
+    int c;
+
+    fp = fopen("file.txt", "r");
+
+    if (fp == NULL) {
+        fprintf(stderr, "File %s failed to open\n", "file.txt");
+        return -1;
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        printf("%c", c);
+    }
+    printf("\n");
+
+    fclose(fp);
+    fp = NULL;
 
     return 0;
 }
